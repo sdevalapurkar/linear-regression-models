c35770299a893ac1eaed688cbc036f85631f5bd3
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2019-02-08 23:20:18 -0800

    add writing to file

--- normal_equation.py
+++ normal_equation.py
@@ -1,6 +1,18 @@
 import numpy as np
 import csv
 
+def write_to_output_file(num_features, w):
+  output_file = open('./outputs/data_100k_300_normal_output.tsv','w+')
+
+  for i in range(1, num_features + 1):
+    output_file.write('w{} \t'.format(i))
+
+  output_file.write('w0 \n')
+
+  for val in w:
+    output_file.write('{}\t'.format(val.item(0)))
+
+
 with open('./dataset/data_100k_300.tsv', 'r') as tsv_input_file:
   tsv_input_file = csv.reader(tsv_input_file, delimiter='\t')
   num_data_points = int(next(tsv_input_file)[0])
@@ -18,5 +30,7 @@ with open('./dataset/data_100k_300.tsv', 'r') as tsv_input_file:
   x = np.matrix(x)
   y = np.matrix(y)
   w = np.matmul(np.linalg.inv(np.matmul(x.transpose(), x)), np.matmul(x.transpose(), y))
+
+  write_to_output_file(num_features, w)
 
   loss = (1 / (2 * num_data_points)) * np.matmul(np.subtract(np.matmul(x, w), y).transpose(), np.subtract(np.matmul(x, w), y))

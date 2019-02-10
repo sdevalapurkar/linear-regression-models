fd65f48c8d146fcc66f39f164ec7f5d3a041667d
Rickus <rickussenekal@gmail.com>
2019-02-10 15:50:49 -0800

    add argparse

--- stochastic_gradient_descent.py
+++ stochastic_gradient_descent.py
@@ -4,9 +4,28 @@ import numpy as np
 import csv
 from tqdm import tqdm
 import time
+import argparse
+
+def constructArguments():
+  # construct the argument parse and parse the arguments
+  parser = argparse.ArgumentParser()
+  parser.add_argument("-e", "--epochs", type=int, default=20,
+    help="# of epochs")
+  parser.add_argument("-l", "--learningRate", type=float, default=0.000001,
+    help="learning rate")
+  parser.add_argument("-b", "--batchSize", type=int, default=1,
+    help="size of SGD batches")
+  parser.add_argument("-i", "--input", type=str, default=None,
+    help="input path of data file")
+  parser.add_argument("-o", "--output", type=str, default=None,
+    help="output path of data file")
+  args = vars(parser.parse_args())
+
+  return args
+
 
 def write_to_output_file(num_features, w):
-  output_file = open('./outputs/data_100k_300_stochastic_output.tsv','w+')
+  output_file = open(args["output"], 'w+')
 
   for i in range(1, num_features + 1):
     output_file.write('w{} \t'.format(i))
@@ -16,15 +35,17 @@ def write_to_output_file(num_features, w):
   for val in w:
     output_file.write('{}\t'.format(val.item(0)))
 
+args = constructArguments()
 
-with open('./dataset/data_100k_300.tsv', 'r') as tsv_input_file:
+with open(args["input"], 'r') as tsv_input_file:
   tsv_input_file = csv.reader(tsv_input_file, delimiter='\t')
   num_data_points = int(next(tsv_input_file)[0])
   num_features = int(next(tsv_input_file)[0])
   next(tsv_input_file, None)
 
-  learning_rate = 0.0000001
-  m = 1
+  learning_rate = args["learningRate"]
+  m = args["batchSize"]
+  epochs = args["epochs"]
   y = []
   x = []
   w = np.random.rand(num_features + 1, 1)
@@ -37,7 +58,7 @@ with open('./dataset/data_100k_300.tsv', 'r') as tsv_input_file:
   x = np.matrix(x)
   y = np.matrix(y)
 
-  for i in range(12):
+  for i in range(epochs):
     D = np.split(x, num_data_points)
     counter = 0
 
